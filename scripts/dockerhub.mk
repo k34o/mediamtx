5b5f330b1f023952bd18311248ff2f0b9ed7ff59
k34o <140044976+k34o@users.noreply.github.com>
2025-08-03 23:33:25 +0900

    Update dockerhub.mk

--- scripts/dockerhub.mk
+++ scripts/dockerhub.mk
@@ -10,28 +10,28 @@ dockerhub:
 
 	docker build --builder=builder \
 	-f docker/ffmpeg-rpi.Dockerfile . \
-	--platform=linux/arm/v6,linux/arm/v7,linux/arm64 \
+	--platform=linux/arm/v5,linux/arm/v6,linux/arm/v7 \
 	-t $(DOCKER_REPOSITORY):$(VERSION)-ffmpeg-rpi \
 	-t $(DOCKER_REPOSITORY):latest-ffmpeg-rpi \
 	--push
 
 	docker build --builder=builder \
 	-f docker/rpi.Dockerfile . \
-	--platform=linux/arm/v6,linux/arm/v7,linux/arm64 \
+	--platform=linux/arm/v5,linux/arm/v6,linux/arm/v7 \
 	-t $(DOCKER_REPOSITORY):$(VERSION)-rpi \
 	-t $(DOCKER_REPOSITORY):latest-rpi \
 	--push
 
 	docker build --builder=builder \
 	-f docker/ffmpeg.Dockerfile . \
-	--platform=linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 \
+	--platform=linux/arm/v5,linux/arm/v6,linux/arm/v7 \
 	-t $(DOCKER_REPOSITORY):$(VERSION)-ffmpeg \
 	-t $(DOCKER_REPOSITORY):latest-ffmpeg \
 	--push
 
 	docker build --builder=builder \
 	-f docker/standard.Dockerfile . \
-	--platform=linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 \
+	--platform=linux/arm/v5,linux/arm/v6,linux/arm/v7 \
 	-t $(DOCKER_REPOSITORY):$(VERSION) \
 	-t $(DOCKER_REPOSITORY):latest \
 	--push
